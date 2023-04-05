817cb065c1dacad7c0e54c5283899ad50911fc3c
bojidaryovchev <blizzardcon10@gmail.com>
2023-04-05 21:57:03 +0300

    usage of wrong env variable

--- src/client/config/parse.config.ts
+++ src/client/config/parse.config.ts
@@ -1,4 +1,4 @@
 import Parse from 'parse';
 
-Parse.initialize(process.env.PARSE_APP_ID, process.env.PARSE_APP_ID);
+Parse.initialize(process.env.PARSE_APP_ID, process.env.PARSE_JS_KEY);
 Parse.serverURL = 'https://parseapi.back4app.com/';
