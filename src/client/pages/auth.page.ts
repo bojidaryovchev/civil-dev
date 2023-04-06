d7a47d79343b973e8755c74c1cd8f3bf135aa53f
bojidaryovchev <blizzardcon10@gmail.com>
2023-04-06 20:44:10 +0300

    extended the file upload example

--- src/client/pages/auth.page.ts
+++ src/client/pages/auth.page.ts
@@ -23,8 +23,35 @@ export class AuthPage extends LitElement {
 
     try {
       const savedFile = await parseFile.save();
+      // Save file metadata to FileMetaData class
+      const FileMetaData = parse.Object.extend('FileMetaData');
+      const fileMetaData = new FileMetaData();
+      fileMetaData.set('file', savedFile);
+      fileMetaData.set('fileName', file.name);
+
+      await fileMetaData.save();
 
       console.log(savedFile);
+    } catch (error) {
+      // handle error
+    }
+  }
+
+  async getFiles(): Promise<void> {
+    const parse = await initParse();
+    const FileMetaData = parse.Object.extend('FileMetaData');
+    const query = new parse.Query(FileMetaData);
+
+    try {
+      const files = await query.find();
+
+      console.log(files);
+
+      files.forEach((fileMetaData) => {
+        const file = fileMetaData.get('file');
+        const fileName = fileMetaData.get('fileName');
+        console.log(`File: ${fileName}, URL: ${file.url()}`);
+      });
     } catch (error) {
       // handle error
     }
@@ -35,6 +62,7 @@ export class AuthPage extends LitElement {
       <p>auth</p>
       <input id="fileInput" type="file" />
       <button type="button" @click="${this.upload}">Upload</button>
+      <button type="button" @click="${this.getFiles}">Get files</button>
     `;
   }
 }
